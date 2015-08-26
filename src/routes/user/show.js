2ebccb6137ba7f8b128a8d21888a25b9ff9768d0
Todd Kronenberg <toddkronenberg@gmail.com>
2015-08-26 12:31:58 -0700

    Forgot to deserialize user workout exercises. Done

--- src/routes/user/show.js
+++ src/routes/user/show.js
@@ -10,6 +10,12 @@ module.exports = (req, res) => {
     if (err || !user) return res.status(500).send('Error getting user');
     console.log('popped user', user);
     user = user.sanitize();
+    // deserialize the user's workout's exercises
+    user.workouts = user.workouts.map((workout) => {
+      return workout.exercises.map((exercise) => {
+        return JSON.parse(exercise);
+      });
+    });
     return res.send({user: user});
   });
 };
